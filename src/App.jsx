f153835ff289044ee02b2c2a80831a86f8053113
mateolohezic <mateolohezic@gmail.com>
2024-02-01 11:43:27 -0300

    Outlert react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, Outlet } from 'react-router-dom'
 import Home from './Pages/Home/Home'
 import Admin from './Pages/Admin/Admin'
 import ProtectedRoute from './Hooks/ProtectedRoute'
 import Login from './Pages/Login/Login'
 import Facturas from './Pages/Facturas/Facturas'
+
+
+
+
+function Dashboard() {
+
+  return (
+    <Outlet />
+  );
+}
+
+function DashboardHome() {
+  return (
+    <div>
+      Home
+      <h2>Este es el home</h2>
+    </div>
+  );
+}
+function DashboardSettings() {
+  return (
+    <div>
+      Setting
+      <h2 className='text-white'>Este es settings</h2>
+    </div>
+  );
+}
+
+
+
+
 
 function App() {
 
@@ -18,6 +49,18 @@ function App() {
         <Route path="/register" element={<ProtectedRoute Component={Home}/>}/>
         <Route path="/admin" element={<ProtectedRoute Component={Admin}/>}/>
         <Route path="/facturas" element={<Facturas/>}/>
+
+
+
+
+
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route index element={<DashboardHome />} />
+          <Route path="settings" element={<DashboardSettings />} />
+        </Route>
+
+
+
       </Routes>
     </>
   )
